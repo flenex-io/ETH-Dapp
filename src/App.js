4054ea785fae683c1a1ac8cb97aa7e52f700f24a
Jagjot Singh <dev.flenex@gmail.com>
2023-08-28 00:12:09 +0530

    Updated the handleSendETH function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import Web3 from 'web3'; // Make sure to import Web3
+
 import './App.css';
 
 const App = () => {
   const [address, setAddress] = useState('');
   const [accounts, setAccounts] = useState([]);
-  const [balance, setBalance] = useState(0);
+  const [balance, setBalance] = useState('0');
 
-  const connectWallet = async () => {
-    if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
-      try {
-        const connectedAccounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setAccounts(connectedAccounts);
-        setAddress(connectedAccounts[0]);
-        getBal(connectedAccounts[0]);
-      } catch (err) {
-        console.error('Connection error:', err);
-      }
-    } else {
-      alert('No wallet detected');
+  const fetchBalance = async (connectedAccount) => {
+    try {
+      const provider = new Web3(window.ethereum);
+      const balanceWei = await provider.eth.getBalance(connectedAccount);
+      const balanceInEther = provider.utils.fromWei(balanceWei, 'ether');
+      setBalance(parseFloat(balanceInEther).toFixed(2));
+    } catch (error) {
+      alert('Error fetching balance:', error);
     }
   };
 
-  const getBal = (connectedAccount) => {
-    window.ethereum.request({ method: 'eth_getBalance', params: [String(connectedAccount), 'latest'] })
-      .then(balance => {
-        const balanceInWei = parseInt(balance, 16);
-        const balanceInEther = balanceInWei / 1e18;
-        const formattedBalance = balanceInEther.toFixed(2);
-        setBalance(formattedBalance);
-      }).catch(error => {
-        console.error('Error fetching balance:', error);
-      });
+
+  const connectWallet = async () => {
+    try {
+      if (window.ethereum) {
+        const connectedAccounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccounts(connectedAccounts);
+        setAddress(connectedAccounts[0]);
+        fetchBalance(connectedAccounts[0]);
+      } else {
+        alert('No wallet detected');
+      }
+    } catch (err) {
+      alert('Connection error:', err);
+    }
   };
 
   const disconnectWallet = () => {
@@ -43,33 +45,39 @@ const App = () => {
     setBalance(0);
   };
 
-  const sendTransaction = async (e) => {
+  const handleSendETH = async (e) => {
     e.preventDefault();
 
-    if(accounts.length === 0){
-      alert("No connected account");
-      return;
-    }
-
-    const toAddress = e.target.to_address.value.trim();
-    if (!/^0x[a-fA-F0-9]{40}$/.test(toAddress)) {
-      alert('Please enter a valid Ethereum address');
-      return;
-    }
-
-    const transactionParams = {
-      from: accounts[0],
-      to: toAddress,
-      gas: Number(21000).toString(16),
-      gasPrice: Number(2500000).toString(16),
-      value: Number(1000000000000000).toString(16),
-    };
-
     try {
-      const result = await window.ethereum.request({ method: 'eth_sendTransaction', params: [transactionParams] });
-      console.log('Transaction result:', result);
-    } catch (err) {
-      console.error('Transaction error:', err);
+      if (!window.ethereum) {
+        alert('MetaMask not detected');
+        return;
+      }
+
+      const provider = new Web3(window.ethereum);
+      const accounts = await provider.eth.getAccounts();
+
+      if (accounts.length === 0) {
+        alert('No connected accounts found');
+        return;
+      }
+
+      const from = accounts[0];
+      const recipientAddress = e.target.to_address.value;
+      const amount = e.target.amount.value;
+
+      await provider.eth.sendTransaction({
+        from,
+        to: recipientAddress,
+        value: provider.utils.toWei(amount, 'ether'),
+        gasPrice: provider.utils.toWei('10', 'gwei'),
+        gas: '21000',
+      });
+
+      alert('Transaction sent successfully');
+      fetchBalance(from);
+    } catch (error) {
+      alert('Error sending transaction:', error);
     }
   };
 
@@ -81,10 +89,10 @@ const App = () => {
           setAccounts(connectedAccounts);
           if (connectedAccounts.length > 0) {
             setAddress(connectedAccounts[0]);
-            getBal(connectedAccounts[0]);
+            fetchBalance(connectedAccounts[0]);
           }
         } catch (err) {
-          console.error('Connection error:', err);
+          alert('Connection error:', err);
         }
       }
     };
@@ -92,27 +100,30 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <div className='connect'>
-        <button type='button' className='button' onClick={connectWallet}>
-          {address
-            ? `Connected: ${address.substring(0, 5).toUpperCase()}...${address.substring(38).toUpperCase()}`
-            : 'Connect Wallet'}
-        </button>
-        <button type='button' className='button' onClick={disconnectWallet}>
-          Disconnect
-        </button>
-      </div>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='connect'>
+          <button type='button' className='button' onClick={connectWallet}>
+            {address
+              ? `Connected: ${address.substring(0, 5).toUpperCase()}...${address.substring(38).toUpperCase()}`
+              : 'Connect Wallet'}
+          </button>
+          <button type='button' className='button' onClick={disconnectWallet}>
+            Disconnect
+          </button>
+        </div>
 
-      <div className='center-container'>
-        <form onSubmit={sendTransaction}>
-          <h3>Enter Address: </h3>
-          <input type='text' name='to_address' placeholder='Address: ' />
-          <h5> <span> Balance: </span>  {balance > 0 ? `${balance}` : '0'} ETH</h5>
-          <input type='submit' value='Submit' />
-        </form>
-      </div>
-    </>
+        <div className='center-container'>
+          <form onSubmit={handleSendETH}>
+            <h3>Enter Address: </h3>
+            <input type='text' name='to_address' placeholder='Address: ' />
+            <input type='number' name='amount' step={0.001} placeholder='Amount (ETH)' />
+            <h5> <span> Balance: </span>  {balance > 0 ? `${balance}` : '0'} ETH</h5>
+            <input type='submit' value='Send ETH' />
+          </form>
+        </div>
+      </header>
+    </div>
   );
 };
 
